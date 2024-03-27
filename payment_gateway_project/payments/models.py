3ee4625c420d146e6c83af1a587ed20c50a9236e
kashishsinghyadav <kashishhsinghhh@gmail.com>
2024-03-27 13:09:18 +0530

    payment gateway integration

---
+++ payment_gateway_project/payments/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Payment(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    razorpay_order_id = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.amount}"
